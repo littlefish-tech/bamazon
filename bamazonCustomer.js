5607872a112da3feaf2fc644018dd4971a871b07
Yimeng Yu <yimeng.yu@gmail.com>
2019-12-15 07:12:04 -0800

    add npm cli

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -11,31 +13,96 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "password",
-  database: "bamazom"
+  database: "bamazon"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  //console.log("connected as id " + connection.threadId);
   // connection.end();
+  
 });
 
-function buy() {
+ 
+function purchaseItems() {
+  connection.query("SELECT * FROM products", function (err, results) {
+
+   
+
+    if (err) throw err;
+    //console.log(results);
+
+     // instantiate
+     var table = new Table({
+      head: ['item_id', 'product_name', "department_name", "price", "stock_quantity"],
+      colWidths: [10, 20, 25, 10, 10],
+      style: {
+        'padding-left': 1
+      , 'padding-right': 1
+      , head: []
+      , border: []
+    }
+    });
+
+    // table is an Array, so you can `push`, `unshift`, `splice` and friends
+    for (i = 0; i < results.length; i++){
+    table.push(
+      [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]
+    );
+    }
+    console.log(table.toString());
+
+
+    //var choiceArray = [];
+    // for (var i = 0; i < results.length; i++){
+      //choiceArray.push(results[i].item_id)
+    //}
+  
     inquirer
-        .prompt({
+        .prompt([
+          {
         name: "selectItem",
         type: "input",
-        message: "Please enter the product ID which you would like to buy.",
-        validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
-        })
+        message: "Please enter the product ID which you would like to buy."
+        },
+        {
+        name: "numbersBuy",
+        type: "input",
+        message: "How many would you like to buy?"
+        }
+        ])
         .then(function(answer){
-            connection.query(
-                
-            )
+            var chosenItem;
+            for(var i = 0; i < results.length; i++){
+              if (results[i].item_id === answer.selectItem){
+                chosenItem = results[i];
+              }
+            }
+            if (chosenItem.stock_quantity > parseInt(answer.numbersBuy)){
+              var updatedQuantity = stock_quantity - parseInt(answer.numbersBuy)
+              connect.query(
+                "UPDATE products SET ? WHERE ?",
+                [{
+                  stock_quantity: updatedQuantity
+                },{
+                  item_id: answer.selectItem
+                }
+                ],
+                function(error) {
+                  if (error) throw err;
+                  console.log("Your order has been placed");
+                  start();
+                }
+              );
+            } else{
+              console.log("We don't have enough stocks");
+          start();
+            }
         })
-}
+      })
+}  
+        purchaseItems()
+        
+                
+            
+        
--- purchase.sql
+++ purchase.sql
@@ -1,7 +1,5 @@
 DROP DATABASE IF EXISTS bamazon;
-
-CREATE DATABASE bamazom;
-
+CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
@@ -12,42 +10,42 @@ CREATE TABLE products (
     stock_quantity INT(50) NULL,
     PRIMARY KEY (item_id)
 );
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("TV", "Eletronic", 750, 80);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("iPhone", "Eletronic", 900, 150);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Peanut Butter", "Food", 6.5, 300);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Apple Juice", "Food", 3, 200);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Hand Cream", "Beauty", 10, 110);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Addidas Running Shoes", "Appreal", 90, 60);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Dish Washer", "Kitchen Appliance", 300, 15);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Pressure Cooker", "Kitchen Appliance", 90, 60);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Chocolate", "Food", 15, 600);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("LEGO", "Kids Toys", 50, 180);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Refrigerator", "Kitchen Appliance", 2150, 10);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Refrigerator", "Kitchen Appliance", 2150, 10);
 
-INCERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("HP Printer", "Office", 2150, 10);
 
