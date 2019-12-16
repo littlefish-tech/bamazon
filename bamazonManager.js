e15ac076a78228de95f09bbf3f871211ce83b258
Yimeng Yu <yimeng.yu@gmail.com>
2019-12-16 10:00:48 -0800

    create bamazonManager.js

---
+++ bamazonManager.js
@@ -0,0 +1,59 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  //console.log("connected as id " + connection.threadId);
+  // connection.end();
+  //purchaseItems()
+  readInventory()
+});
+
+function checkInventory()
+inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View Products for Sale",
+        "View Low Inventory",
+        "Add to Inventory",
+        "Add New Product"
+      ]
+    })
+    .then(function(answer){
+        switch (answer.action) {
+            case "View Products for Sale":
+                viewProducts();
+                break;
+
+            case "View Low Inventory":
+                viewLowInventory();
+                break;
+
+            case "Add to Inventory":
+                addInventory();
+                break;
+            
+            case "Add New Product":
+                addNewProduct();
+                break;
+
+        }
+    })
