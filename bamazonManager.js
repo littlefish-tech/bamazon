c4c14772ce5c9b86810f926bc7b70d4f80fc06a3
Yimeng Yu <yimeng.yu@gmail.com>
2019-12-17 07:26:32 -0800

    add view stock and low inventory feature

--- bamazonManager.js
+++ bamazonManager.js
@@ -21,14 +21,20 @@ connection.connect(function (err) {
   //console.log("connected as id " + connection.threadId);
   // connection.end();
   //purchaseItems()
-  readInventory()
+  checkInventory()
 });
 
-function checkInventory()
+function checkInventory(){
+  connection.query("SELECT * FROM products", function (err, results) {
+    //console.log(results);
+    
+    if (err) throw err;
+    var totalLength = results.length;
 inquirer
-    .prompt({
+    .prompt([
+      {
       name: "action",
-      type: "rawlist",
+      type: "list",
       message: "What would you like to do?",
       choices: [
         "View Products for Sale",
@@ -36,7 +42,8 @@ inquirer
         "Add to Inventory",
         "Add New Product"
       ]
-    })
+    }
+  ])
     .then(function(answer){
         switch (answer.action) {
             case "View Products for Sale":
@@ -57,3 +64,63 @@ inquirer
 
         }
     })
+  })
+}
+
+function viewProducts() {
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    //console.log(results);
+
+    // instantiate
+    var table = new Table({
+      head: ['item_id', 'product_name', "department_name", "price", "stock_quantity"],
+      colWidths: [10, 20, 25, 10, 10],
+      style: {
+        'padding-left': 1
+        , 'padding-right': 1
+        , head: []
+        , border: []
+      }
+    });
+
+    // table is an Array, so you can `push`, `unshift`, `splice` and friends
+    for (i = 0; i < results.length; i++) {
+      table.push(
+        [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]
+      );
+    }
+    console.log(table.toString());
+  })
+  checkInventory()
+}
+    function viewLowInventory() {
+      connection.query("SELECT * FROM products", function(err, results) {
+        //console.log(results)
+        var lowStockItem;
+       lowStockItem = results.filter(item => item.stock_quantity < 100 );
+       //console.log(lowStockItem);
+
+       var table = new Table({
+        head: ['item_id', 'product_name', "department_name", "price", "stock_quantity"],
+        colWidths: [10, 20, 25, 10, 10],
+        style: {
+          'padding-left': 1
+          , 'padding-right': 1
+          , head: []
+          , border: []
+        }
+      });
+  
+      // table is an Array, so you can `push`, `unshift`, `splice` and friends
+      for (i = 0; i < lowStockItem.length; i++) {
+        table.push(
+          [lowStockItem[i].item_id, lowStockItem[i].product_name, lowStockItem[i].department_name, lowStockItem[i].price, lowStockItem[i].stock_quantity]
+        );
+      }
+      console.log(table.toString());
+        
+      })
+      checkInventory()
+    }
+  
